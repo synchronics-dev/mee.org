613cbcd5e09aa09d12b96595d19f0e4b73babc81
synchronics-dev <sizasmalls@gmail.com>
2021-04-25 08:53:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+let ourButton = document.querySelector('button');
+let writeUp = document.querySelector('.write-up');
+let isWriteUpOpen = false;
+
+
+ourButton.addEventListener('click', function() {
+    if (!isWriteUpOpen) {
+        writeUp.style.height = 'auto';
+        isWriteUpOpen = true;
+    } else {
+        writeUp.style.height = '0';
+        isWriteUpOpen = false;
+    }
+});
