4d914c33cc66475f557e7caf4b34d2a7b7dcb01d
synchronics-dev <sizasmalls@gmail.com>
2021-04-26 09:28:29 +0100

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-
-
-let ourButton = document.querySelector('button');
-let writeUp = document.querySelector('.write-up');
+let ourButton = document.querySelector("button");
+let writeUp = document.querySelector(".write-up");
 let isWriteUpOpen = false;
 
-
-ourButton.addEventListener('click', function() {
-    if (!isWriteUpOpen) {
-        writeUp.style.height = 'auto';
-        isWriteUpOpen = true;
-    } else {
-        writeUp.style.height = '0';
-        isWriteUpOpen = false;
-    }
+ourButton.addEventListener("click", function () {
+  if (!isWriteUpOpen) {
+    writeUp.style.height = "auto";
+    isWriteUpOpen = true;
+  } else {
+    writeUp.style.height = "0";
+    isWriteUpOpen = false;
+  }
 });
